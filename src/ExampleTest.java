6744ba007c9b489852b634483ee5847360d68aad
Бушуев Роман <it.romanit@bankon24.su>
2022-08-10 23:17:21 +1000

    Добавлен класс Franc, новый тест выполняется

--- src/ExampleTest.java
+++ src/ExampleTest.java
@@ -10,6 +10,13 @@ public class ExampleTest {
     }
 
     @org.junit.jupiter.api.Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @org.junit.jupiter.api.Test
     public void testEquality() {
         assertTrue(new Dollar(5). equals(new Dollar(5)));
         assertFalse(new Dollar(5). equals(new Dollar(6)));
