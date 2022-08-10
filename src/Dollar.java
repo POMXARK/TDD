672c6b3111d47231c6a7ff294ed0c2f3915bc79b
Бушуев Роман <it.romanit@bankon24.su>
2022-08-11 01:29:43 +1000

    Рефакторинг

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,13 +1,14 @@
 public class Dollar extends Money {
     Dollar(int amount) {
         this.amount = amount;
+        currency = "USD";
     }
     Money times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
 
     String currency() {
-        return "USD";
+        return currency;
     }
-
+    private String currency;
 }
