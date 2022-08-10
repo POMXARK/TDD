d6db5545cd550501eb07537e648aa1bf8cea06ed
Бушуев Роман <it.romanit@bankon24.su>
2022-08-10 21:36:32 +1000

    Рефакторинг класса Dollar, тест работает

--- src/Dollar.java
+++ src/Dollar.java
@@ -5,8 +5,7 @@ public class Dollar {
         this.amount = amount;
     }
     Dollar times(int multiplier) {
-        amount *= multiplier;
-        return null;
+        return new Dollar(amount * multiplier);
     }
 
 }
