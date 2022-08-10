750e756b1b7264f918bad126750e8a3a8b473c1b
Бушуев Роман <it.romanit@bankon24.su>
2022-08-10 22:33:56 +1000

    Тест работает

--- src/ExampleTest.java
+++ src/ExampleTest.java
@@ -14,6 +14,7 @@ public class ExampleTest {
     @org.junit.jupiter.api.Test
     public void testEquality() {
         assertTrue(new Dollar(5). equals(new Dollar(5)));
+        assertFalse(new Dollar(5). equals(new Dollar(6)));
     }
 
 }
