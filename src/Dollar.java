62d409f0850c17397fd52477dc7fd11ff3b153d7
Бушуев Роман <it.romanit@bankon24.su>
2022-08-11 01:44:05 +1000

    Рефакторинг тестов, все тесты работают

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,14 +1,10 @@
 public class Dollar extends Money {
-    Dollar(int amount) {
-        this.amount = amount;
-        currency = "USD";
-    }
-    Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
+    Dollar(int amount, String currency) {
+        super(amount, currency);
     }
 
-    String currency() {
-        return currency;
+    Money times(int multiplier) {
+        return Money.dollar(amount * multiplier);
     }
-    private String currency;
+
 }
