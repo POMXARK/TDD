4b496131e68258038ad36adc99f7ce201d61390b
Бушуев Роман <it.romanit@bankon24.su>
2022-08-12 23:01:51 +1000

    Тест выполнен testPlusReturnsSum

--- src/Sum.java
+++ src/Sum.java
@@ -1,4 +1,8 @@
-public class Sum {
+public class Sum implements Expression{
+    Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
     Money augend;
     Money addend;
 
